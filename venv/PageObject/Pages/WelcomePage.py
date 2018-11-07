1c7ba13e56575b2680d7dfe26e263383a5deb4c3
katerynapolonska <kapolo19961996@gmail.com>
2018-11-07 14:34:24 +0200

    Testing new US

---
+++ venv/PageObject/Pages/WelcomePage.py
@@ -0,0 +1,9 @@
+class Welcome(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def SignIn(self):
+        self.driver.find_element_by_id("Sign_in").click()
+
+    def SignUp(self):
+        self.driver.find_element_by_id("Sign_up").click()
